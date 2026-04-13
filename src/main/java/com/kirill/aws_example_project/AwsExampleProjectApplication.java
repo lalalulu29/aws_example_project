a204f6848eaf59eacbe422e48b3e86191a423942
Kirill Lyubarskikh <k.lyubarskikh@gmail.com>
2026-04-13 21:51:03 +0200

    Initial commit

---
+++ src/main/java/com/kirill/aws_example_project/AwsExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kirill.aws_example_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsExampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirill/aws_example_project/AwsExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirill.aws_example_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
